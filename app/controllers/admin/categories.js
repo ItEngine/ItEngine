a49710b430a93c1b8955171f8588d0d1b51296b2
mapeveri <martinpeveri@gmail.com>
2016-01-14 23:52:35 -0300

    Finalización del abm de categorías

--- app/controllers/admin/categories.js
+++ app/controllers/admin/categories.js
@@ -1,35 +1,79 @@
 'use strict'
 
+//Model user
+const Category = require("../../models/category.js");
+
 module.exports = {
 
   //Show admin
   index: function(req, res) {
-    console.log("index");
+    try {
+      Category.find({}, function(err, data){
+        return res.render("admin/categories/categories", {
+          is_admin: true,
+          data: data
+        });
+      });
+    } catch (e) {
+      return res.render("500");
+    }
   },
 
   //Display form new category
   newcategory: function(req, res){
-    console.log("newcategory");
+    return res.render("admin/categories/categorycreate", {
+      is_admin: true
+    });
   },
 
   //Save new record category
   insert: function(req, res){
-    console.log("insert");
+    let idcateg = req.body.idcateg;
+    let name = req.body.name;
+
+    try {
+      let category = new Category({idcateg: idcateg, name: name});
+      category.save(function(){
+        res.redirect("/admin/categories");
+      });
+    } catch (e) {
+      return res.render("500");
+    }
   },
 
   //Delete record category
   delete: function(req, res){
-    console.log("delete");
+    Category.remove({ _id : req.params.id}, function (err) {
+        res.redirect("/admin/categories");
+    });
   },
 
   //Display form updatecategory
   updatecategory: function(req, res){
-    console.log("updatecategory");
+    let id = req.params.id;
+    Category.findOne({_id: id},function(err, doc){
+        if (err){
+          return res.render("500");
+        }
+        return res.render("admin/categories/categoryupdate", {
+          is_admin: true,
+          doc: doc
+        });
+    });
   },
 
   //Update category
   update: function(req, res){
-    console.log("update");
+    let idcateg = req.body.idcateg;
+    let name = req.body.name;
+
+    Category.findOneAndUpdate({idcateg: idcateg}, {name: name},function(err, category){
+      if(err){
+        return res.render("500");
+      }
+
+      res.redirect("/admin/categories");
+    });
   },
 
 }
